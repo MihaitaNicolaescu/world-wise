96baa7e84f88e69904fff40e77670a941d6dad84
Mihăiță Nicolaescu <32862807+MihaitaNicolaescu@users.noreply.github.com>
2025-10-25 23:33:52 +0300

    feat: Update homepage styles and content for improved layout and design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,10 @@ function Home () {
     <div className={styles.homepage}>
         <PageNav />
         <div className={styles.content}>
-          <h1 className='text-red-500 text-5xl'>World Wise</h1>
-          <h2>Track your journey. Discover your world.</h2>
-          <h3>Welcome to World Wise Track your travels and see where your packages—and you—have been. Let’s start exploring the world together!</h3>
-          <Link to="/app">Go to Appe</Link>
+          <h2 className="text-[75px]">Track your journey</h2>
+          <h2 className="text-[85px] font-bold mt-[-50px]">Discover your world</h2>
+          <h3 className="w-2/3 mx-auto text-[35px] font-semibold text-center">Welcome to World Wise Track your travels and see where your packages and you have been. Let’s start exploring the world together!</h3>
+          <Link className="w-fit mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 mt-5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" to="/app">START TRAKING NOW</Link>
         </div>
     </div>
   )
